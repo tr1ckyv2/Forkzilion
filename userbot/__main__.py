67899c32b52f50814078656e3a0a46bfe749679b
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-05-13 22:07:44 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -41,12 +41,24 @@ except PhoneNumberInvalidError:
 
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
-
+    
+    
+try:
+    LOGS.info("Initiating InlineBot....")
+    inlinebot = TelegramClient("inlinebot", API_KEY, API_HASH).start(bot_token=BOT_TOKEN)
+    bot.loop.run_until_complete(add_bot(BOT_USERNAME))
+    LOGS.info("InlineBot is online now")
+except Exception as e:
+    LOGS.info("InlineBot Failed .")
+    LOGS.info("InlineBot is quiting...")
+    LOGS.info(str(e))
+    
 LOGS.info("You are running Project Fizilion")
 
 LOGS.info(
     "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
     "If you need assistance, head to https://t.me/ProjectFizilionChat")
+
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
