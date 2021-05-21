068008e7d937eaf79696a0e05420041d88c57078
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-05-21 22:36:42 +0530

    Update gdrive.py

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -231,13 +231,8 @@ async def download(gdrive, service, uri=None):
         required_file_name = None
     if uri:
         full_path = os.getcwd() + TEMP_DOWNLOAD_DIRECTORY.strip(".")
-        if isfile(uri) and uri.endswith(".torrent"):
-            downloads = aria2.add_torrent(
-                uri, uris=None, options={"dir": full_path}, position=None
-            )
-        else:
-            uri = [uri]
-            downloads = aria2.add_uris(uri, options={"dir": full_path}, position=None)
+        uri = [uri]
+        downloads = aria2.add_uris(uri, options={"dir": full_path}, position=None)
         gid = downloads.gid
         await check_progress_for_dl(gdrive, gid, previous=None)
         file = aria2.get_download(gid)
