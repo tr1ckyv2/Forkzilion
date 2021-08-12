c01f57a4ad0b077303ac8b160235566a41ad2a31
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-12 15:29:33 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -106,7 +106,7 @@ async def pingme(pong):
     await pong.edit("`Pong!`")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(`"Pong!`\n**Ping:**`%sms`" % (duration))
+    await pong.edit("`Pong!`\n**Ping:**`%sms`" % (duration))
 
 CMD_HELP.update(
     {
