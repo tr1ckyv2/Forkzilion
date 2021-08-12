2e1cd16a4491286e5651e3b7130e506dd88a12a3
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-12 15:27:22 +0700

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -231,9 +231,6 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern=r"^.(alive|on)$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    start = datetime.now()
-    end = datetime.now()
-    ms = (end - start).microseconds / 1000
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
         f"`=================================`\n"
