3623312a4b1d5e301f50fd9cb698bd71cfb7fd0d
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-12 16:04:22 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -6,7 +6,6 @@
 """ Userbot module containing commands related to the \
     Information Superhighway (yes, Internet). """
 
-import time
 from datetime import datetime
 
 from speedtest import Speedtest
@@ -99,6 +98,32 @@ async def neardc(event):
         f"This Datacenter : `{result.this_dc}`"
     )
 
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
 
 @register(outgoing=True, pattern="^.ping$")
 async def pingme(pong):
