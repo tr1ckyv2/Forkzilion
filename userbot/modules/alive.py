1352021e68f3e69cc64216b074f5886e012c1774
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-12 16:53:30 +0700

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -237,7 +237,7 @@ async def amireallyalive(alive):
         f"  `Forkzilion is up and running...` \n"
         f"`================================= `\n"
         f"           **Informations:**        \n"
-        f"• `Master         : {DEFAULTUSER}
+        f"• `Master         :` {DEFAULTUSER} \n"
         f"• `Running On     : Ubuntu 20.04.1`\n"
         f"• `Forkzilion Ver : Custom `\n"
         f"• `Python         : {python_version()} `\n"   
