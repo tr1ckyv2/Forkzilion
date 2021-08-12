c019679e262f097f07a54003edf7d42b27b4aa93
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-12 16:36:20 +0700

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -249,6 +249,17 @@ async def amireallyalive(alive):
         f"`=================================`\n"
 
     )
+    if ALIVE_LOGO:
+        try:
+            logo = ALIVE_LOGO
+            msg = await bot.send_file(alive.chat_id, logo, caption=output, del_in=10)
+            await alive.delete()
+            await sleep(30)
+        except BaseException:
+            await alive.edit(
+                output + "\n\n *`The provided logo is invalid."
+                "\nMake sure the link is directed to the logo picture`"
+            )
     msg = await alive.edit(output)
     await sleep(30)
         
