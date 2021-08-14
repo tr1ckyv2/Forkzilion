f65c342206acbbe9add1d755418b646659c155fb
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-14 13:57:44 +0700

    Update stats.py

--- userbot/modules/stats.py
+++ userbot/modules/stats.py
@@ -94,23 +94,21 @@ async def stats(event):
         unread += dialog.unread_count
     stop_time = time.time() - start_time
     full_name = inline_mention(await event.client.get_me())
-    response = f"📌 ✨ Stats of **Master** ~ {full_name}** ✨ \n\n"
-    response += f"✨ **Private Chats**: {private_chats} ✨ \n"
-    response += f"   ✨ `Users: {private_chats - bots}` ✨ \n"
-    response += f"   ✨ `Bots: {bots}` ✨ \n"
-    response += f"✨ **Groups**: {groups} ✨ \n"
-    response += f"✨ **Channels**: {broadcast_channels} ✨ \n"
-    response += f"✨ **Admin in Groups:** {admin_in_groups} ✨ \n"
-    response += f"   ✨ `Creator: {creator_in_groups}` ✨ \n"
-    response += f"   ✨ `Admin Rights: {admin_in_groups - creator_in_groups}` ✨ \n"
-    response += f"✨ **Admin in Channels**: {admin_in_broadcast_channels} ✨ \n"
-    response += f"   ✨ `Creator: {creator_in_channels}` ✨ \n"
-    response += (
-        f"   ✨ `Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` ✨ \n"
-    )
-    response += f"✨ **Unread**: {unread} ✨ \n"
-    response += f"✨ **Unread Mentions**: {unread_mentions} ✨ \n\n"
-    response += f"📌 __It Took:__ {stop_time:.02f}s \n"
+    response = f"📌  Stats of **Master** ~ {full_name}** \n\n"
+    response += f" • **Private Chats:** `{private_chats}` \n"
+    response += f"    ° `Users: {private_chats - bots}` \n"
+    response += f"    ° `Bots: {bots}` \n"
+    response += f" • **Groups:** `{groups}` \n"
+    response += f" • **Channels:** `{broadcast_channels}` \n"
+    response += f" • **Admin in Groups:** `{admin_in_groups}` \n"
+    response += f"    ° `Creator: {creator_in_groups}`  \n"
+    response += f"    ° `Admin Rights: {admin_in_groups - creator_in_groups}` \n"
+    response += f" • **Admin in Channels:** `{admin_in_broadcast_channels}` \n"
+    response += f"    ° `Creator: {creator_in_channels}`  \n"
+    response += f"    ° `Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` \n"
+    response += f" • **Unread:** `{unread}` \n"
+    response += f" • **Unread Mentions:** `{unread_mentions}` \n\n"
+    response += f"⚡ __It Took:__ {stop_time:.02f}s \n"
     await stat.edit(response)
 
 
