e13be4bba9e0d7ca4834bcbc13fb9b4d0d6d8e8a
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-18 12:35:41 +0700

    Update info plugin

--- userbot/modules/info.py
+++ userbot/modules/info.py
@@ -18,7 +18,7 @@ from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
 
 
-@register(pattern=".info(?: |$)(.*)", outgoing=True)
+@register(pattern="^\.info(?: |$)(.*)", outgoing=True)
 async def who(event):
 
     await event.edit(
