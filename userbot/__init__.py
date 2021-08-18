4bc73e7b56a7b29c7e0661d72c93a95950af7d0b
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-18 18:36:49 +0700

    Change terminal username

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -118,7 +118,7 @@ TZ_NUMBER = os.environ.get("TZ_NUMBER") or 1
 USERBOT_VERSION = os.environ.get("USERBOT_VERSION") or "production 3.1+"
 
 # User Terminal alias
-USER_TERM_ALIAS = "Ubuntu-20.04"
+USER_TERM_ALIAS = "Ubuntu-21.04"
 
 # Clean Welcome
 CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME") or "True")
