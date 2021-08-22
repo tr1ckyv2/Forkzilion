0e616d1a5f2c21fcfa1b63eebc4ebb4eaab8c007
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-22 11:00:51 +0700

    Fix bugs

--- termux_install.sh
+++ termux_install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 clear
 echo -E "                                ******,*,.          ...       "
 echo -E "                           /////*****,*... .  .               "
@@ -34,27 +35,27 @@ clear
 pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/mkaraniya/OpenUserBot.git
-cd OpenUserBot
+git clone https://github.com/DunggVN/Forkzilion -b DunggVN /Fizilion
+cd Fizilion
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
 
 mv sample_config.env config.env
 
-mkdir -p $PREFIX/var/lib/postgresql
-initdb $PREFIX/var/lib/postgresql
-pg_ctl -D $PREFIX/var/lib/postgresql start
+mkdir -p "$PREFIX/var/lib/postgresql"
+initdb "$PREFIX/var/lib/postgresql"
+pg_ctl -D "$PREFIX/var/lib/postgresql" start
 createdb botdb
 createuser botuser
 
-cd ..
+cd .. || exit
 echo "pg_ctl -D $PREFIX/var/lib/postgresql start" > startbot.sh
 echo "cd OpenUserBot" >> startbot.sh
 echo "python3 -m userbot" >> startbot.sh
 chmod 755 startbot.sh
 
 echo "Done."
-echo "Now edit config.env with nano or anything you want, then run the userbot with startbot.sh"
-echo "Please edit the db to postgresql://botuser:@localhost:5432/botdb"
+echo "Now fill vars config.env, then run the userbot with startbot.sh"
+echo "Notes: Create a postgresql database url then fill DATABASE_URL with it"
 echo "Good luck!"
